5e4a5663cf1bcdf56441f4e292b9c5355b031aa1
Evan <epleon36@gmail.com>
2025-05-12 18:12:53 -0400

    Live video stream works

--- src/web.py
+++ src/web.py
@@ -1,11 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template, Response
+from camera import PlantCamera
 app = Flask(__name__)
+
+plantCam = PlantCamera()
 
 @app.route("/")
 
+def index():
+    return render_template('index.html')
 
-def helloworld():
-    return "Hello World!"
+def gen(camera):
+    """Video streaming generator function."""
+    yield b'--frame\r\n'
+    while True:
+        frame = camera.get_frame()
+        yield b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n--frame\r\n'
+
+
+@app.route('/video_feed')
+def video_feed():
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(gen(plantCam),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 if __name__ == "__main__":
