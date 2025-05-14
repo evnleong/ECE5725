9e05a00c23d050ad03961bae04d5a89f6293d2fa
Evan <epleon36@gmail.com>
2025-05-14 11:18:15 -0400

    Update endpoints for remote light increase and decrease

--- src/web.py
+++ src/web.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response,jsonify
 from camera import PlantCamera
 app = Flask(__name__)
-
-plantCam = PlantCamera()
+import atexit
+import time
+from threading import Thread
+from globals import plant_data
+from light import GrowLight
 
 @app.route("/")
 
@@ -23,6 +26,43 @@ def video_feed():
     return Response(gen(plantCam),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
+@app.route('/data')
+def get_data():
+    return jsonify(plant_data)
 
-if __name__ == "__main__":
-    app.run()
+
+@app.route('/light/increase', methods=['POST'])
+def increase_light():
+    growLight.increaseDutyCycle()
+    return jsonify({'status': 'success', 'dutyCycle': growLight.dutyCycle})
+
+@app.route('/light/decrease', methods=['POST'])
+def decrease_light():
+    growLight.decreaseDutyCycle()
+    return jsonify({'status': 'success', 'dutyCycle': growLight.dutyCycle})
+
+
+# @app.route('/pump_on', methods=['POST'])
+# def pump_on():
+#     print ("Pump remotely on")
+#     waterPump.pump_on()
+#     return jsonify({'status': 'success', 'pumpStatus': waterPump.state})
+
+
+@atexit.register
+def shutdown_camera():
+    print("Shutting down camera...")
+    plantCam.close()
+
+
+def start_flask_thread(camera,light):
+    global plantCam,growLight
+    plantCam = camera
+    growLight = light
+    thread = Thread(target=lambda: app.run(threaded=True))
+    thread.daemon = True
+    thread.start()
+
+# if __name__ == "__main__":
+   
+#     app.run()
