ec9a58f647f76421a1047807faf3db10509ecd30
Evan <epleon36@gmail.com>
2025-05-14 11:30:22 -0400

    Finalize GUI, add pump functionality

--- src/main.py
+++ src/main.py
@@ -4,14 +4,20 @@ import time,datetime
 import os
 import sys
 import RPi.GPIO as GPIO
-from threading import Thread
+from threading import Thread, Event
 from camera import PlantCamera
 from light import GrowLight
 from thsensor import THSensor
+from pump import PlantPump
+from msensor import MSensor,detect_moisture
+import web
+from globals import plant_data
+
+stop_event = Event()
 
 # Environment Setup 
 os.putenv('SDL_VIDEODRV','fbcon')
-os.putenv('SDL_FBDEV', '/dev/fb0')
+os.putenv('SDL_FBDEV', '/dev/fb1')
 os.putenv('SDL_MOUSEDRV','dummy')
 os.putenv('SDL_MOUSEDEV','/dev/null')
 os.putenv('DISPLAY','')
@@ -22,10 +28,7 @@ GPIO.setup(22, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP) 
 GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-#Pump control pin
-pumpPin = 21
-
+GPIO.setup(6,GPIO.OUT,initial = GPIO.HIGH)
 
 #Colours 
 WHITE = (255,255,255)
@@ -41,17 +44,25 @@ appHeight = 240
 #Date & Time 
 date = datetime.datetime.today().strftime('%Y-%m-%d %H:%M:%S')
 
+
+#Fonts + Text
 font_big = pygame.font.Font(None, 35)
 font_med = pygame.font.Font(None, 20)
 font_sm = pygame.font.Font(None,15)
-text_surface = font_sm.render("Live Camera", True, (DARK_GREEN))
-text_surface2 = font_big.render("Welcome!", True, (DARK_GREEN))
+video_title_text = font_sm.render("Live Camera", True, (DARK_GREEN))
+welcome_text= font_big.render("Welcome!", True, (DARK_GREEN))
+enter_text = font_med.render("Press Button #17 to Enter", True, (DARK_GREEN))
 text_surface3 = font_big.render("Temp:", True, (DARK_GREEN))
 timestamp_text = font_sm.render(f"Date: {date} " , True, (DARK_GREEN))
+brightup_text = font_sm.render(f"< Brightness Up" , True, (DARK_GREEN))
+brightdown_text = font_sm.render(f"< Brightness Down" , True, (DARK_GREEN))
+pump_text = font_sm.render(f"< Increase Humidity" , True, (DARK_GREEN))
+quit_text = font_sm.render(f"<  Quit" , True, (DARK_GREEN))
+stats_text = font_med.render(f"Greenhouse Statistics" , True, (DARK_GREEN))
  
 
 screen = pygame.display.set_mode((appWidth, appHeight))
-
+plantCamSuccess = True
 # camera = Picamera2()
 
 code_run = True
@@ -61,17 +72,25 @@ try:
 
 except:
     code_run= False
+    plantCamSuccess =False
 
-growLight = GrowLight(pin=20)
-thSensor = THSensor(pin=16)
+growLight = GrowLight(pin=16)
+thSensor = THSensor(pin=20)
 
+try:
+    waterPump = PlantPump(pin = 6)
+except Exception as e:
+    raise e
+mSensor = MSensor(pin=26)
 
 # Assets 
-
 sun_icon = pygame.transform.scale(pygame.image.load(os.path.join('assets','sun.png')), (40,40))
 temp_icon = pygame.transform.scale(pygame.image.load(os.path.join('assets','temp.png')), (50,50))
 water_icon= pygame.transform.scale(pygame.image.load(os.path.join('assets','water.png')), (50,50))
 
+menu_level_1 =  True
+menu_level_2 = False
+start_preview = False
 
 # Callbacks
 
@@ -82,47 +101,80 @@ def button23(channel):
       growLight.decreaseDutyCycle()
 
 def button22(channel):
-      print("button22")
+      print("pump on")
+      waterPump.pump_on()
+    
 
 def button17(channel):
-      print("button17")
+    print('Entering Main Menu')
+    global menu_level_1, menu_level_2, start_preview
+    menu_level_1 = False
+    menu_level_2 = True
+    start_preview = True
+    screen.fill((LIGHT_GREEN))
+    pygame.display.update()
 
 # Add event listeners
 GPIO.add_event_detect(27, GPIO.RISING, callback = button27, bouncetime =200)
 GPIO.add_event_detect(23, GPIO.RISING, callback = button23, bouncetime =200)
-GPIO.add_event_detect(22, GPIO.RISING, callback = button22, bouncetime =200)
+GPIO.add_event_detect(22, GPIO.RISING, callback = button22, bouncetime =700)
 GPIO.add_event_detect(17, GPIO.RISING, callback = button17, bouncetime =200)
 
-menu_level_1 =  True
-menu_level_2 = False
-start_preview = False
 
 
 def temphumidityThread():
-    humidity,temperature = thSensor.update_ht()
-    text_humidity = font_big.render(f"{humidity}%", True, (255, 255, 255))
-    text_temperature = font_big.render(f"{temperature}*F", True, (255, 255, 255))
-    screen.blit(text_humidity,(100,100))
-    screen.blit(text_temperature,(100,200))
-    
-  
-   
-def camThread():
-
-    while (code_run):
-        curr_time = time.time()
+    while code_run:
         try:
-            plantCam.generate_preview(screen)
-        except:
-            pass
+            text_light_level = font_med.render(f"Light Level: {growLight.dutyCycle}%",True,(DARK_GREEN) )
+            light_level_rect = text_light_level.get_rect(center=(230,50))
+
+
+            humidity,temperature = thSensor.update_ht()
+            text_humidity = font_med.render(f"Humidity: {humidity}%", True, (DARK_GREEN))
+            text_temperature = font_med.render(f"Temp: {temperature}*F", True, (DARK_GREEN))
+            humidity_rect = text_humidity.get_rect(center=(230,150))
+            temperature_rect = text_temperature.get_rect(center=(225,100))
+            moisture = plant_data['moisture']
+            text_moisture = font_med.render(f"Moisture: {moisture}", True, (DARK_GREEN))
+            moisture_rect = text_moisture.get_rect(center=(230,180))
+       
+            screen.fill(LIGHT_GREEN,humidity_rect)
+            screen.fill(LIGHT_GREEN,temperature_rect)
+            screen.fill(LIGHT_GREEN,light_level_rect)
+            screen.fill(LIGHT_GREEN,moisture_rect)
+            pygame.display.update()
+            screen.blit(text_humidity,humidity_rect)
+            screen.blit(text_temperature,temperature_rect)
+            screen.blit(text_light_level,light_level_rect)
+            screen.blit(text_moisture,moisture_rect)
+            time.sleep(1)
+
+            plant_data['temperature'] = temperature
+            plant_data['humidity'] = humidity
+            plant_data['light_level'] = growLight.dutyCycle
+            # plant_data['moisture'] = moisture 
+            plant_data['pump_status'] = waterPump.state
+        except Exception as e:
+            print(e)
+    
+
+   
+# def camThread():
+
+#     while code_run:
+#         curr_time = time.time()
+#         try:
+#             plantCam.generate_preview(screen)
+#         except:
+#             pass
 
 def timestampThread():
 
-    while (code_run):
+    while code_run:
         try:
             date = datetime.datetime.today().strftime('%H:%M:%S %Y-%m-%d')
             timestamp_text= font_sm.render(f"Current Time: {date} " , True, (DARK_GREEN))
-            timestamp_rect = timestamp_text.get_rect(center=(90,10))
+            timestamp_rect = timestamp_text.get_rect(center=(90,230))
             screen.fill(LIGHT_GREEN,timestamp_rect)
             pygame.display.update()
             screen.blit(timestamp_text,timestamp_rect)
@@ -132,7 +184,10 @@ def timestampThread():
 
 curr_time = time.time()
 init_time = time.time()
-while (code_run and curr_time < init_time + 10):
+flask_thread_started = False
+
+
+while (code_run and curr_time < init_time + 360):
     curr_time = time.time()
 
     events=pygame.event.get()
@@ -141,57 +196,60 @@ while (code_run and curr_time < init_time + 10):
         if (e.type == pygame.KEYDOWN and e.key == pygame.K_SPACE):
             print('Quit Callback')
             code_run = False 
-            # filename = 'image.jpg'
-            # print('Capturing frame...')
-            # camera.switch_mode_and_capture_file(configStill, filename)
-            
-            # print('Displaying the captured frame for 5 seconds...')
-            # capturedFrame = pygame.image.load(filename).convert()
-            # capturedFrameRectangle = capturedFrame.get_rect()
-            # screen.blit(capturedFrame, capturedFrameRectangle)
-            # pygame.display.update()
-            # time.sleep(5)
+          
 
     if menu_level_1:
         screen.fill((LIGHT_GREEN))
-        screen.blit(text_surface2,(100,100))
-        for e in events:
-            if(e.type is MOUSEBUTTONUP):
-                x,y = pygame.mouse.get_pos()
-                text_surface3 = font_big.render(f' Touch at ({x},{y}) ', True, (DARK_GREEN))
-                rect1 = text_surface3.get_rect(center=(160,100))
-                screen.blit(text_surface3, rect1)                     
-                if ( y > 200 ):       
-                    print('entering menu 2')
-                    menu_level_1 = False
-                    menu_level_2 = True
-                    start_preview = True
-                    screen.fill((LIGHT_GREEN))
-                    pygame.display.update()
+        screen.blit(welcome_text,(100,100))
+        screen.blit(enter_text,(80,160))
+        # for e in events:
+        #     if(e.type is MOUSEBUTTONUP):
+        #         x,y = pygame.mouse.get_pos()
+        #         text_surface3 = font_big.render(f' Touch at ({x},{y}) ', True, (DARK_GREEN))
+        #         rect1 = text_surface3.get_rect(center=(160,100))
+        #         screen.blit(text_surface3, rect1)                     
+        #         if ( y > 200 ):       
+        #             print('entering menu 2')
+        #             menu_level_1 = False
+        #             menu_level_2 = True
+        #             start_preview = True
+        #             screen.fill((LIGHT_GREEN))
+        #             pygame.display.update()
                     
     elif menu_level_2: 
 
         if (start_preview):
             #blit once when preview starts--
-            screen.blit(text_surface,(40,80))
-            screen.blit(sun_icon,(200,50))
-            screen.blit(temp_icon,(200,100))
-            screen.blit(water_icon,(200,160))
+            screen.fill((LIGHT_GREEN))
+            screen.blit(sun_icon,(130,25))
+            screen.blit(temp_icon,(130,75))
+            screen.blit(water_icon,(130,145))
+            screen.blit(brightup_text,(10,0))
+            screen.blit(brightdown_text,(10,60))
+            screen.blit(pump_text,(10,120))
+            screen.blit(quit_text,(10,190))
+            screen.blit(stats_text,(145,10))
+            if not flask_thread_started:
+                web.start_flask_thread(plantCam,growLight)
+                flask_thread_started = True
+            elif flask_thread_started: #if button 17 pressed again, quit
+                code_run = False
             start_preview = False
-            t1 = Thread(target=camThread)
+            t1 = Thread(target=temphumidityThread)
             t1.start()
             t2 = Thread(target=timestampThread)
             t2.start()
     pygame.display.update()
 code_run = False # update code_run to false on timeout
-    
 screen.fill((LIGHT_GREEN))
+growLight.light_off()
 pygame.display.update()
-plantCam.close()
-if t1.is_alive():
-    t1.join()
-if t2.is_alive():
-    t2.join()
+if plantCamSuccess:
+    plantCam.close()
+# if t1.is_alive():
+#     t1.join()
+# if t2.is_alive():
+#     t2.join()
 pygame.quit()
 GPIO.cleanup()
 del(pitft) # stop touch monitoring
